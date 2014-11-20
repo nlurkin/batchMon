215268f8fe6142e6f7cf76f3261d1adf8b9d3650
nlurkin <ncl@hep.ph.bham.ac.uk>
2014-11-20 17:42:22 +0000

    Client submit remote backtrace when exception on submitting

--- batchClient.py
+++ batchClient.py
@@ -127,7 +127,10 @@ def argParser():
     server = Pyro4.Proxy(uri)
     
     if args.config:
-        server.addBatch(os.getcwd()+"/"+args.config, args.name, args.queue, args.test, args.keep)
+        try:
+            server.addBatch(os.getcwd()+"/"+args.config, args.name, args.queue, args.test, args.keep)
+        except Exception:
+            print "".join(Pyro4.util.getPyroTraceback())
     
     if args.submit:
         server.submitInit(args.name)
