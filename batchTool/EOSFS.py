5d5b28c7a691b0c92fad55228dacf3479e2bbbe3
nlurkin <nicolas.lurkin@gmail.com>
2015-01-24 12:59:14 +0100

    Small bug fixes

--- batchTool/EOSFS.py
+++ batchTool/EOSFS.py
@@ -9,7 +9,7 @@ import subprocess
 import datetime
 import os
 
-_preloaded = (None, [])
+_preloaded = [None, []]
 _preloadTime = None
 
 def isFile():
@@ -20,6 +20,7 @@ def isDirectory():
 
 def exists(path, preload):
     global _preloaded
+    global _preloadTime
     path = os.path.abspath(path)
     #Preload if requested
     if preload:
@@ -34,9 +35,10 @@ def exists(path, preload):
     else:
         #Don't preload
         #Check if path already preloaded and if preloaded information has not expired (1 min time)
-        if os.path.dirname(path)==_preloaded[0] and (datetime.datetime.now()-_preloadTime)<datetime.timedelta(minutes=5):
+        if os.path.dirname(path)==_preloaded[0] and (datetime.datetime.now()-_preloadTime)<datetime.timedelta(minutes=1):
             #It is preloaded and not expired
             ret = (os.path.basename(path) in _preloaded[1])
+            return ret
         else:
             #Path has not been preloaded or is expired. Check normally
             ret = subprocess.call(["/afs/cern.ch/project/eos/installation/0.3.15/bin/eos.select", "ls", path])
