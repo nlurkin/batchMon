cc437d0cda876a9bb5ebd2a4cffab48c3c46ddf9
nlurkin <nicolas.lurkin@cern.ch>
2017-08-25 12:25:51 +0100

    Save and use previous position in list

--- batchTool/pyroObjects.py
+++ batchTool/pyroObjects.py
@@ -244,11 +244,13 @@ class DisplayClient(object):
     def deleteBatch(self, index):
         if(index>=len(self.batchList)):
             return None
+        self.lastIndex = index
         return self.batchList[index]['name']
     
     def selectBatch(self, index):
         if(index>=len(self.batchList)):
             return None
+        self.lastIndex = index
         self.batchName = self.batchList[index]['name']
         return self.batchName
     
