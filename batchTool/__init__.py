b2aaa1795dd8231183ed74b83bc216724b1ffb83
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2017-09-30 17:15:33 +0200

    Update __init__

--- batchTool/__init__.py
+++ batchTool/__init__.py
@@ -1,7 +1,7 @@
 __version__ = '3.0'
 
 from config import ConfigBatch, BatchToolExceptions
-from monitor2 import Monitor2
+from monitor import MonitorHTCondor, MonitorLSF
 from display2 import Display2
 from pyroObjects import JobServer, DisplayClient
 from util import printDebug, _debugLevel
