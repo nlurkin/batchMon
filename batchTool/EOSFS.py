0842d8aadef8f1c99abba87168399019b4ab391d
nlurkin <nicolas.lurkin@gmail.com>
2017-12-13 09:55:25 +0100

    Preload non-existing directories, man-switch between eosuser/eosna62

--- batchTool/EOSFS.py
+++ batchTool/EOSFS.py
@@ -24,13 +24,18 @@ def exists(path, preload):
     path = os.path.abspath(path)
     #Preload if requested
     if preload:
-        p = subprocess.Popen(["/afs/cern.ch/project/eos/installation/0.3.15/bin/eos.select", "ls", path], stdout=subprocess.PIPE)
+        #p = subprocess.Popen(["/usr/bin/eos", "root://eosuser.cern.ch/", "ls", path], stdout=subprocess.PIPE)
+        p = subprocess.Popen(["/usr/bin/eos", "root://eosna62.cern.ch/", "ls", path], stdout=subprocess.PIPE)
         (out, _) = p.communicate();
         ret = p.returncode
         #If ret!=0, path do not exist. Cannot preload 
         if ret==0:
             _preloaded[0] = path
             _preloaded[1] = out.splitlines()
+            _preloadTime = datetime.datetime.now()
+        else:
+            _preloaded[0] = path
+            _preloaded[1] = []
             _preloadTime = datetime.datetime.now()
     else:
         #Don't preload
@@ -41,12 +46,14 @@ def exists(path, preload):
             return ret
         else:
             #Path has not been preloaded or is expired. Check normally
-            ret = subprocess.call(["/afs/cern.ch/project/eos/installation/0.3.15/bin/eos.select", "ls", path])
+            #ret = subprocess.call(["/usr/bin/eos", "root://eosuser.cern.ch/", "ls", path])
+            ret = subprocess.call(["/usr/bin/eos", "root://eosna62.cern.ch/", "ls", path])
     return ret==0
 
 def mkDir(path):
     if not exists(path, False):
-        return subprocess.call(["/afs/cern.ch/project/eos/installation/0.3.15/bin/eos.select", "mkdir", path])
+       #return subprocess.call(["/usr/bin/eos", "root://eosuser.cern.ch/", "mkdir", path])
+       return subprocess.call(["/usr/bin/eos", "root://eosna62.cern.ch/", "mkdir", path])
 
 def rmTree(path):
     pass
