849e5a784b829aaadac518e2c6aaa37ff048bed7
nlurkin <nlurkin@eplt19.ph.bham.ac.uk>
2017-12-13 10:10:50 +0100

    Dynamic EOS mgm url

--- batchTool/EOSFS.py
+++ batchTool/EOSFS.py
@@ -18,14 +18,22 @@ def isFile():
 def isDirectory():
     pass
 
+def getMGMUrl(path):
+    if "/eos/experiment" in path:
+        return "root://eosna62.cern.ch/"
+    else:
+        return "root://eosuser.cern.ch/"
+    
 def exists(path, preload):
     global _preloaded
     global _preloadTime
     path = os.path.abspath(path)
     #Preload if requested
+        
+    eos_mgm_url = getMGMUrl(path)
+    
     if preload:
-        #p = subprocess.Popen(["/usr/bin/eos", "root://eosuser.cern.ch/", "ls", path], stdout=subprocess.PIPE)
-        p = subprocess.Popen(["/usr/bin/eos", "root://eosna62.cern.ch/", "ls", path], stdout=subprocess.PIPE)
+        p = subprocess.Popen(["/usr/bin/eos", eos_mgm_url, "ls", path], stdout=subprocess.PIPE)
         (out, _) = p.communicate();
         ret = p.returncode
         #If ret!=0, path do not exist. Cannot preload 
@@ -47,13 +55,12 @@ def exists(path, preload):
         else:
             #Path has not been preloaded or is expired. Check normally
             #ret = subprocess.call(["/usr/bin/eos", "root://eosuser.cern.ch/", "ls", path])
-            ret = subprocess.call(["/usr/bin/eos", "root://eosna62.cern.ch/", "ls", path])
+            ret = subprocess.call(["/usr/bin/eos", eos_mgm_url, "ls", path])
     return ret==0
 
 def mkDir(path):
     if not exists(path, False):
-       #return subprocess.call(["/usr/bin/eos", "root://eosuser.cern.ch/", "mkdir", path])
-       return subprocess.call(["/usr/bin/eos", "root://eosna62.cern.ch/", "mkdir", path])
+        return subprocess.call(["/usr/bin/eos", getMGMUrl(path), "mkdir", path])
 
 def rmTree(path):
     pass
